bc427c94b87830ceaf60111e57f42643a0ccc319
JensOAndersen <jensoleand@gmail.com>
2019-05-16 14:20:04 +0200

    added support for more letters

--- ConsoleWriter/Program.cs
+++ ConsoleWriter/Program.cs
@@ -11,9 +11,44 @@ namespace ConsoleWriter
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string[]> letters = GetLettersFromFile();
+            Dictionary<string, string[]> letters = new Dictionary<string, string[]>();
 
+            try
+            {
+                string path = @"C:\Users\josa\source\repos\S1M3F2019_Demos\ConsoleWriter\letters.png";
+                int height = 5;
+                int width = 5;
+                string alphabet = "abcdefghijklmnopqrstuvwxyz!?,.';:=-)(<> 1234567890[]#\"/\\|^*_";
 
+                if (args.Length == 1)
+                {
+                    path = args[0];
+                }
+
+                if (args.Length == 2)
+                {
+                    path = args[0];
+                    alphabet = args[3];
+                }
+
+                if (args.Length == 4)
+                {
+                    path = args[0];
+                    alphabet = args[3];
+                    height = int.Parse(args[1]);
+                    width = int.Parse(args[2]);
+                }
+
+                //change path, width and height
+                letters = GetLettersFromFile(path, height, width, alphabet);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occured, press enter to exit");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Environment.Exit(0);
+            }
 
             string input = Console.ReadLine().ToLower();
 
@@ -29,6 +64,25 @@ namespace ConsoleWriter
                 }
             }
 
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("C:/output/words.txt"))
+                {
+                    foreach (var item in output)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Application exited with following code: ");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("press any key to exit");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
             foreach (var item in output)
             {
                 Console.WriteLine(item);
@@ -37,17 +91,12 @@ namespace ConsoleWriter
             Console.ReadLine();
         }
 
-        static Dictionary<string, string[]> GetLettersFromFile()
+        static Dictionary<string, string[]> GetLettersFromFile(string path, int lHeight, int lWidth, string alphabet)
         {
-            //!!!!!CHANGE THIS PATH!!!!!
-            string path = @"C:\Users\josa\source\repos\S1M3F2019_Demos\ConsoleWriter\letters.jpg";
-
-            //Letters in the same order as the letters in letters.jpg
-            string alphabet = "abcdefghijklmnopqrstuvwxyz ";
 
             //Constants for letter height and width, change if letters.jpg changes
-            int letterWidth = 5;
-            int letterHeight = 5;
+            int letterWidth = lHeight;
+            int letterHeight = lWidth;
 
             Dictionary<string, string[]> output = new Dictionary<string, string[]>();
 
@@ -70,7 +119,8 @@ namespace ConsoleWriter
                             if (pixel.R < 50)
                             {
                                 letterLines[h] += letter.ToUpper();
-                            } else
+                            }
+                            else
                             {
                                 letterLines[h] += " ";
                             }
@@ -78,7 +128,7 @@ namespace ConsoleWriter
                         xPos++;
                     }
 
-                    output.Add(letter, letterLines);
+                    output.Add(@letter, letterLines);
                 }
             }
 
