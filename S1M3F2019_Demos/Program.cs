ce950ef2a1cabc9aad7da34a2a0bd14fd679eaab
JensOAndersen <jensoleand@gmail.com>
2019-05-20 09:34:53 +0200

    updated demos with example of methods

--- S1M3F2019_Demos/Program.cs
+++ S1M3F2019_Demos/Program.cs
@@ -7,7 +7,28 @@ namespace S1M3F2019_Demos
 
         static void Main(string[] args)
         {
-            int[] myArr = new int[] { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(MyAdder(20, 5));
+
+            Console.WriteLine(MultiplyMyName(5,"Jens-Ole"));
+
+            Console.ReadLine();
+        }
+        //access modifier - keyword (optional) - returtype - method name - parameters - method body
+        static int MyAdder(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        static string MultiplyMyName(int times, string name)
+        {
+            string n = "";
+
+            for (int i = 0; i < times; i++)
+            {
+                n += name;
+            }
+
+            return n;
         }
     }
 }
