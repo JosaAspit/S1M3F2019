4cbbab7515d2070333dea4ffd5588835b58a8ef1
JensOAndersen <jensoleand@gmail.com>
2019-05-22 10:46:54 +0200

    Added another demo of classes :))

---
+++ ClassyClasses/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassyClasses
+{
+    public class Person
+    {
+        //Instance members
+        public string name;
+        public string occupation;
+        public DateTime birthday;
+
+        public Person(string name, string occupation, DateTime birthday)
+        {
+            this.name = name;
+            this.occupation = occupation;
+            this.birthday = birthday;
+        }
+
+        public string GetSummary()
+        {
+            return $"{name} was born in {birthday.Year}, and is working as a {occupation}";
+        }
+
+        public static Person NewPerson(string name, string occupation, DateTime birthday)
+        {
+            return new Person(name, occupation, birthday);
+        }
+    }
+}
---
+++ ClassyClasses/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassyClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Jens-Ole", "Teacher", new DateTime(1989, 08, 10));
+            Person person2 = new Person("Hans Karl", "Brick Slayer", new DateTime(1960, 03, 17));
+            Person person3 = Person.NewPerson("Hanne", "Financial manager", new DateTime(1974, 10, 13));
+            //person.name = "Jens-Ole";
+            //person.occupation = "Teacher";
+            //person.birthday = new DateTime(1989, 08, 10);
+
+            List<Person> people = new List<Person>();
+            
+            people.Add(person1);
+            people.Add(person2);
+            people.Add(person3);
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.GetSummary());
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
